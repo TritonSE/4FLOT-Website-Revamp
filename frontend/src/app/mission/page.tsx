09bf81cbdbb518b1c320d9342b5e3d633e4626d2
katelynpdn <94756838+katelynpdn@users.noreply.github.com>
2024-02-11 14:31:53 -0800

    Text and adding images

--- frontend/src/app/mission/page.tsx
+++ frontend/src/app/mission/page.tsx
@@ -6,13 +6,27 @@ import styles from "./page.module.css";
 
 export default function Mission() {
   return (
-    <main>
+    <main className={styles.page}>
       <BackgroundHeader
         backgroundImage="/mission_background.png"
-        header="Our Mission"
+        header="OUR MISSION"
         title="Why We Do It"
         description="Leading the way for generations to come! Together we can .... make a difference by paying it forward with Love, Compassion, and Community Outreach for all humanity."
       />
+      <div className={styles.text}>
+        <div className={styles.subtitle}>We Pay It Forward With...</div>
+      </div>
+
+      <div className={styles.text}>
+        <div className={styles.subtitle}>Here&apos;s Our Story</div>
+        <p className={styles.description}>
+          At one point or another, each of the founding members have gone through personal
+          struggles, some have experienced homelessness, hunger, medical illnesses and others
+          juggled single parenting, while furthering their education, and so on. However, the common
+          denominator was that each of us needed Help. So now we are &quot;The Helpers&quot; 4
+          Future Leaders of Tomorrow -because the people we help are our future.
+        </p>
+      </div>
     </main>
   );
 }
