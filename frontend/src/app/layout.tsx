79d4910d1e5315b1d9da7242f7aad2cf37aefe87
jennymar <jenmar528@gmail.com>
2024-02-11 18:16:56 -0800

    Added nav bar to layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 
 import type { Metadata } from "next";
 import "./globals.css";
+import { HeaderBar } from "@/components/HeaderBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +14,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <HeaderBar />
+        {children}
+      </body>
     </html>
   );
 }
