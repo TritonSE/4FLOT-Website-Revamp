a78d9db78b6163a30d6e65bbe7335484673c0356
kevindo0720 <80845738+kevindo0720@users.noreply.github.com>
2024-02-13 11:14:49 -0800

    finished we pay it forward part

--- frontend/src/app/mission/page.tsx
+++ frontend/src/app/mission/page.tsx
@@ -13,10 +13,36 @@ export default function Mission() {
         title="Why We Do It"
         description="Leading the way for generations to come! Together we can .... make a difference by paying it forward with Love, Compassion, and Community Outreach for all humanity."
       />
+
+      {/* We pay it forward - KEVIN */}
       <div className={styles.text}>
-        <div className={styles.subtitle}>We Pay It Forward With...</div>
+        <div className={styles.titlelarge}>We Pay It Forward With...</div>
+        <div className={styles.rectangleContainer}>
+          <div className={styles.customRectangle}>
+            <div className={styles.circleWhite}>
+              <img src="/threepeople.png" alt="Image 1" className={styles.centeredImage} />
+            </div>
+            <div className={styles.RectangleTitle}>Service</div>
+            <div className={styles.BodytextInsideRectangle}>Lorem ipsum dolor sit amet consectetur. Et vestibulum enim nunc ultrices. Donec blandit sollicitudin vitae integer mauris sed. Mattis duis id viverra suscipit morbi.</div>
+          </div>
+          <div className={styles.customRectangle}>
+            <div className={styles.circleWhite}>
+              <img src="/handheart.png" alt="Image 2" className={styles.centeredImage} />
+            </div>
+            <div className={styles.RectangleTitle}>Compassion</div>
+            <div className={styles.BodytextInsideRectangle}>Lorem ipsum dolor sit amet consectetur. Et vestibulum enim nunc ultrices. Donec blandit sollicitudin vitae integer mauris sed. Mattis duis id viverra suscipit morbi.</div>
+          </div>
+          <div className={styles.customRectangle}>
+            <div className={styles.circleWhite}>
+              <img src="/puzzle.png" alt="Image 3" className={styles.centeredImage} />
+            </div>
+            <div className={styles.RectangleTitle}>Community</div>
+            <div className={styles.BodytextInsideRectangle}>Lorem ipsum dolor sit amet consectetur. Et vestibulum enim nunc ultrices. Donec blandit sollicitudin vitae integer mauris sed. Mattis duis id viverra suscipit morbi.</div>
+          </div>
+        </div>
       </div>
 
+      {/* OUR STORY - KATELYN */}
       <div className={styles.text}>
         <div className={styles.subtitle}>Here&apos;s Our Story</div>
         <p className={styles.description}>
@@ -27,6 +53,8 @@ export default function Mission() {
           Future Leaders of Tomorrow -because the people we help are our future.
         </p>
       </div>
+
+
     </main>
   );
 }
