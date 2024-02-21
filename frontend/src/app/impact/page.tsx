87ff62273ac9c01cd76bc090db8ce65ca42ae09a
Sophia Zhu <sophiazhu16@gmail.com>
2024-02-20 16:34:32 -0800

    match frontend to figma

--- frontend/src/app/impact/page.tsx
+++ frontend/src/app/impact/page.tsx
@@ -37,27 +37,27 @@ export default function Impact() {
         color: "black",
       }}
     >
-      <div className="flex flex-col px-24 pt-10 pb-20">
-        <h1 className="text-4xl font-bold mb-5">Read Our Stories</h1>
-        <p className="mb-10">
+      <div className="flex flex-col px-24 pt-24 pb-20">
+        <h1 className="text-5xl font-bold mb-5">Read Our Stories</h1>
+        <p className="text-2xl mb-16">
           A nonprofit is as strong as the community that holds it up. Together, we can do more than
           we can do alone. Let&apos;s bring our abilities and passions together to make real change.
           Your donations will help feed and clothes our underprivileged and underserved communities.
         </p>
-        <div className="flex flex-wrap gap-5 gap-y-10">
+        <div className="flex flex-wrap gap-x-5 gap-y-14">
           {testimonials.map((testimonial) => (
             <TestimonialCard key={testimonial._id} testimonial={testimonial} />
           ))}
         </div>
 
-        <h1 className="text-4xl font-bold mt-10 mb-5">Where We&apos;ve Been</h1>
-        <p className="mb-10">
+        <h1 className="text-4xl font-bold mt-36 mb-5">Where We&apos;ve Been</h1>
+        <p className="text-2xl mb-20">
           A nonprofit is as strong as the community that holds it up. Together, we can do more than
           we can do alone. Let&apos;s bring our abilities and passions together to make real change.
           Your donations will help feed and clothes our underprivileged and underserved communities.
         </p>
 
-        <div className="flex flex-wrap gap-5 justify-between mb-20">
+        <div className="flex flex-wrap gap-x-20 gap-y-14 mb-64">
           {events.map((event) => (
             <EventCard key={event._id} testimonial={event} />
           ))}
--- frontend/src/components/testimonialCard.tsx
+++ frontend/src/components/testimonialCard.tsx
@@ -14,7 +14,7 @@ const TestimonialCard = ({ testimonial }: CardProps) => {
       <main className="testimonial-card-container">
         <Image src={testimonial.image} alt="image" width={400} height={200} />
         <div className="testimonial-text-container">
-          <h1>{testimonial.title}</h1>
+          <h1>&#8220;{testimonial.title}&#8221;</h1>
           <p>{testimonial.description}</p>
         </div>
       </main>
