22a18de3622946d76946acc77f95aa8984703eee
jennymar <jenmar528@gmail.com>
2024-02-23 00:28:02 -0800

    mission page CSS fixes

--- frontend/src/app/mission/page.tsx
+++ frontend/src/app/mission/page.tsx
@@ -3,13 +3,13 @@ import React from "react";
 
 import BackgroundHeader from "../../components/BackgroundHeader";
 import Button from "../../components/Button";
-import ValueCard from "../../components/valueCard";
+import ValueCard from "../../components/ValueCard";
 
 import styles from "./page.module.css";
 
 export default function Mission() {
   return (
-    <main className={styles.page}>
+    <main>
       <BackgroundHeader
         backgroundImage="/mission_background.png"
         header="OUR MISSION"
@@ -17,47 +17,53 @@ export default function Mission() {
         description="Leading the way for generations to come! Together we can .... make a difference by paying it forward with Love, Compassion, and Community Outreach for all humanity."
       />
 
-      {/* We pay it forward*/}
-      <div className={styles.text}>
-        <div className={styles.titlelarge}>We Pay It Forward With...</div>
-        <div className={styles.rectangleContainer}>
-          <ValueCard
-            title="Service"
-            iconSrc="/threepeople.svg"
-            description="Lorem ipsum dolor sit amet consectetur. Et vestibulum enim nunc ultrices. Donec
+      <div className={styles.page}>
+        {/* We pay it forward*/}
+        <div className={styles.text}>
+          <div className={styles.titlelarge}>We Pay It Forward With...</div>
+          <div className={styles.rectangleContainer}>
+            <ValueCard
+              title="Service"
+              iconSrc="/threepeople.svg"
+              description="Lorem ipsum dolor sit amet consectetur. Et vestibulum enim nunc ultrices. Donec
               blandit sollicitudin vitae integer mauris sed. Mattis duis id viverra suscipit morbi."
-          ></ValueCard>
-          <ValueCard
-            title="Compassion"
-            iconSrc="/handheart.svg"
-            description="Lorem ipsum dolor sit amet consectetur. Et vestibulum enim nunc ultrices. Donec
+            ></ValueCard>
+            <ValueCard
+              title="Compassion"
+              iconSrc="/handheart.svg"
+              description="Lorem ipsum dolor sit amet consectetur. Et vestibulum enim nunc ultrices. Donec
               blandit sollicitudin vitae integer mauris sed. Mattis duis id viverra suscipit morbi."
-          ></ValueCard>
-          <ValueCard
-            title="Community"
-            iconSrc="/puzzle.svg"
-            description="Lorem ipsum dolor sit amet consectetur. Et vestibulum enim nunc ultrices. Donec
+            ></ValueCard>
+            <ValueCard
+              title="Community"
+              iconSrc="/puzzle.svg"
+              description="Lorem ipsum dolor sit amet consectetur. Et vestibulum enim nunc ultrices. Donec
               blandit sollicitudin vitae integer mauris sed. Mattis duis id viverra suscipit morbi."
-          ></ValueCard>
+            ></ValueCard>
+          </div>
         </div>
-      </div>
 
-      {/* OUR STORY*/}
-      <div className={styles.text}>
-        <div className={styles.titlelarge}>Here&apos;s Our Story</div>
-        <p className={styles.description}>
-          At one point or another, each of the founding members have gone through personal
-          struggles, some have experienced homelessness, hunger, medical illnesses and others
-          juggled single parenting, while furthering their education, and so on. However, the common
-          denominator was that each of us needed Help. So now we are &quot;The Helpers&quot; 4
-          Future Leaders of Tomorrow -because the people we help are our future.
-        </p>
-        <Button text="Meet Our Team" link="/team"></Button>
-      </div>
-      <div className={styles.imageContainer}>
-        <Image src={"/mission_top_left.png"} alt="Story image 1" width="234" height="195" />
-        <Image src={"/mission_top_right.png"} alt="Story image 2" width="266" height="285" />
-        <Image src={"/mission_bottom.png"} alt="Story image 3" width="532" height="298" />
+        {/* OUR STORY*/}
+        <div className={styles.storyContainer}>
+          <div className={styles.storyText}>
+            <div className={styles.titlelarge}>Here&apos;s Our Story</div>
+            <p className={styles.description}>
+              At one point or another, each of the founding members have gone through personal
+              struggles, some have experienced homelessness, hunger, medical illnesses and others
+              juggled single parenting, while furthering their education, and so on. However, the
+              common denominator was that each of us needed Help. So now we are &quot;The
+              Helpers&quot; 4 Future Leaders of Tomorrow -because the people we help are our future.
+            </p>
+            <Button text="Meet Our Team" link="/team"></Button>
+          </div>
+          <div className={styles.imageContainer}>
+            <div className={styles.imageContainerTopRow}>
+              <Image src={"/mission_top_left.png"} alt="Story image 1" width="234" height="195" />
+              <Image src={"/mission_top_right.png"} alt="Story image 2" width="266" height="285" />
+            </div>
+            <Image src={"/mission_bottom.png"} alt="Story image 3" width="532" height="298" />
+          </div>
+        </div>
       </div>
     </main>
   );
--- frontend/src/components/valueCard.tsx
+++ frontend/src/components/valueCard.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import styles from "./valueCard.module.css";
+import styles from "./ValueCard.module.css";
 
 type ValueCardProps = {
   iconSrc: string;
