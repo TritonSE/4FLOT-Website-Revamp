d341921566ea505aa28b50997cdff02954f6f2d3
Jack Hansen <47556286+jackavh@users.noreply.github.com>
2024-03-12 16:11:35 -0700

    adds monetary/physical selector

--- frontend/src/components/payment/DonateApp.tsx
+++ frontend/src/components/payment/DonateApp.tsx
@@ -1,16 +1,21 @@
 "use client";
+
+import { useState } from "react";
+
 import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 
 import DonateCard from "./DonateCard";
 import DonateCardText from "./DonateCardText";
 import DonateForm from "./DonateForm";
+import DonationTypeSelection from "./DonationTypeSelection";
 
 type DonateAppProps = {
   className: string;
 };
 
 export default function DonateApp({ className }: DonateAppProps) {
-  console.log(process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID);
+  const [monetary, setMonetary] = useState(true);
+  console.log(monetary ? "monetary" : "physical");
   return (
     <div className={className}>
       <PayPalScriptProvider
@@ -23,6 +28,7 @@ export default function DonateApp({ className }: DonateAppProps) {
       >
         <DonateCard>
           <DonateCardText />
+          <DonationTypeSelection monetary={monetary} setMonetary={setMonetary} />
           <DonateForm />
         </DonateCard>
       </PayPalScriptProvider>
