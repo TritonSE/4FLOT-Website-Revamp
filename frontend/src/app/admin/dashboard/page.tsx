5e284ab5cd9b6c8e6204a8c73679d8f66142882c
wrachel <Rwei951@gmail.com>
2024-04-05 10:18:27 -0700

    frontend cards

--- frontend/src/app/admin/dashboard/page.tsx
+++ frontend/src/app/admin/dashboard/page.tsx
@@ -1,1 +1,40 @@
 // Admin Dashboard landing page
+import DashboardCard from "@/components/DashboardCard";
+
+import styles from "./page.module.css";
+
+export default function Dashboard() {
+    
+    return (
+      <main className={styles.page}>
+          <div className={styles.cards}>
+            <DashboardCard
+              imageURI="/dashboard_eventcreator.png"
+              url="/admin/eventcreator"
+              title="Event Creator"
+              last_updated="Month XX, XXXX, XX:XX"
+            />
+            <DashboardCard
+              imageURI="/dashboard_pageeditor.png"
+              url="/admin/pageeditor"
+              title="Page Editor"
+              last_updated="Month XX, XXXX, XX:XX"
+            />
+            <DashboardCard
+              imageURI="/dashboard_newsletter.png"
+              url="/admin/newsletter"
+              title="Newsletter"
+              last_updated="Month XX, XXXX, XX:XX"
+            />
+            <DashboardCard
+              imageURI="/dashboard_mailinglist.png"
+              url="/admin/mailinglist"
+              title="Mailing List"
+              last_updated="Month XX, XXXX, XX:XX"
+            />
+            
+          </div>
+      </main>
+    );
+  }
+  
--- frontend/src/app/admin/pageeditor/page.tsx
+++ frontend/src/app/admin/pageeditor/page.tsx
@@ -1,1 +1,47 @@
 // Admin Page Editor landing page
+// Admin Dashboard landing page
+import DashboardCard from "@/components/DashboardCard";
+
+import styles from "./page.module.css";
+
+export default function Dashboard() {
+    
+    return (
+      <main className={styles.page}>
+          <div className={styles.cards}>
+            <DashboardCard
+              imageURI="/impact_bg.png"
+              url="/admin/home"
+              title="Home"
+              last_updated="Month XX, XXXX, XX:XX"
+            />
+            <DashboardCard
+              imageURI="/impact_bg.png"
+              url="/admin/about"
+              title="About Us"
+              last_updated="Month XX, XXXX, XX:XX"
+            />
+            <DashboardCard
+              imageURI="/impact_bg.png"
+              url="/admin/involved"
+              title="Get Involved"
+              last_updated="Month XX, XXXX, XX:XX"
+            />
+            <DashboardCard
+              imageURI="/impact_bg.png"
+              url="/admin/impact"
+              title="Our Impact"
+              last_updated="Month XX, XXXX, XX:XX"
+            />
+            <DashboardCard
+              imageURI="/impact_bg.png"
+              url="/admin/donate"
+              title="Donate"
+              last_updated="Month XX, XXXX, XX:XX"
+            />
+            
+          </div>
+      </main>
+    );
+  }
+  
