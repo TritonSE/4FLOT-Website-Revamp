93136c99374c9b2a9b8cae15b50af7aafa641770
katelynpdn <94756838+katelynpdn@users.noreply.github.com>
2024-04-10 13:38:32 -0700

    Dynamic Header Bar

--- frontend/src/app/admin/layout.tsx
+++ frontend/src/app/admin/layout.tsx
@@ -1,11 +1,9 @@
-import AdminHeaderBar from "@/components/AdminHeaderBar";
 import NavigationBar from "@/components/NavigationBar";
 import "./globals.css";
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   return (
     <section>
-      <AdminHeaderBar />
       <NavigationBar />
       {children}
     </section>
