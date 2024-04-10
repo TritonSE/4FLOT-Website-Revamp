55a5ed58c838c00cd5b9cf0bdc1006e5cc26493f
katelynpdn <94756838+katelynpdn@users.noreply.github.com>
2024-04-09 20:09:28 -0700

    Admin header bar

--- frontend/src/components/PageEditorCard.tsx
+++ frontend/src/components/PageEditorCard.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-import styles from "./EventsCard.module.css";
+import styles from "./DashboardCards.module.css";
 
 // TODO: Change props to include className and change to an EventDetails type
 type CardProps = {
@@ -10,28 +10,28 @@ type CardProps = {
   title: string;
   last_updated: string;
   imageURI: string;
-  url:string;
+  url: string;
 };
 
 // TODO: Make class names responsive
 const PageEditorCard = ({ title, last_updated, imageURI, url }: CardProps) => {
-    return (
-      <main className={styles.cardContainer} style={{ width: `310px`, height: `340px` }}>
-        <div style={{ width: "310px", height: "340px", overflow: "hidden" }}>
-          <Image src={imageURI} alt="image" width={310} height={340} />
-        </div>
+  return (
+    <main className={styles.cardContainer} style={{ width: `310px`, height: `340px` }}>
+      <div style={{ width: "310px", height: "340px", overflow: "hidden" }}>
+        <Image src={imageURI} alt="image" width={310} height={340} />
+      </div>
 
-        <div className={styles.homeCardContent}>
-          <div className={styles.textContainer}>
-            <div className={styles.cardTitle}>{title}</div>
-            <div className={styles.homeCardDescription}>{`Last Updated: ${last_updated}`}</div>
-          </div>
-          <Link href={`./${url}`}>
-            <button className={styles.cardButton}>Open Editor</button>
-          </Link>
+      <div className={styles.homeCardContent}>
+        <div className={styles.textContainer}>
+          <div className={styles.cardTitle}>{title}</div>
+          <div className={styles.homeCardDescription}>{`Last Updated: ${last_updated}`}</div>
         </div>
-      </main>
-    );
+        <Link href={`./${url}`}>
+          <button className={styles.cardButton}>Open Editor</button>
+        </Link>
+      </div>
+    </main>
+  );
 };
 
 export default PageEditorCard;
