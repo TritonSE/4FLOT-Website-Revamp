894dc22edcb94a131beccd7166f9a39c762667ec
katelynpdn <94756838+katelynpdn@users.noreply.github.com>
2024-04-10 13:38:32 -0700

    Dynamic Header Bar

--- frontend/src/components/NavigationBar.tsx
+++ frontend/src/components/NavigationBar.tsx
@@ -6,88 +6,108 @@ import React, { useState } from "react";
 import styles from "./NavigationBar.module.css";
 
 const NavigationBar = () => {
-  const [activeMenu, setActiveMenu] = useState<string>();
+  const [activeMenu, setActiveMenu] = useState<string>("Dashboard");
 
   const handleOnClick = (menuDiv: string) => {
     setActiveMenu(menuDiv);
   };
   return (
-    <div className={styles.navBar}>
-      <Image
-        width={168.25}
-        height={59.69}
-        src="footerLogo.svg"
-        alt="4 Future Leaders of Tomorrow Logo"
-      />
-      <div className={styles.menu}>
-        <div className={activeMenu === "dashboard" ? styles.menuActive : styles.menuDiv}>
-          <Image width={18} height={18} src="dashboardIcon.svg" alt="Dashboard Icon" />
-          <Link
-            href="/admin/dashboard"
-            onClick={() => {
-              handleOnClick("dashboard");
-            }}
-          >
-            Dashboard
-          </Link>
+    <main>
+      <div className={styles.headerBar}>
+        <div className={styles.title}>
+          <p>{activeMenu}</p>
+          {activeMenu !== "Dashboard" && (
+            <div className={styles.pageDesc}>
+              <p>Dashboard &gt;</p>
+              <p style={{ font: "var(--font-body-bold)", color: "var(--color-primary-purple)" }}>
+                {activeMenu}
+              </p>
+            </div>
+          )}
         </div>
-        <div className={activeMenu === "eventCreator" ? styles.menuActive : styles.menuDiv}>
-          <Image width={18} height={18} src="calendarIcon.svg" alt="Calendar Icon" />
-          <Link
-            href="#"
-            onClick={() => {
-              handleOnClick("eventCreator");
-            }}
-          >
-            Event Creator
-          </Link>
-        </div>
-        <div className={activeMenu === "pageeditor" ? styles.menuActive : styles.menuDiv}>
-          <Image width={18} height={18} src="pageIcon.svg" alt="Page Icon" />
-          <Link
-            href="/admin/pageeditor"
-            onClick={() => {
-              handleOnClick("pageeditor");
-            }}
-          >
-            Page Editor
-          </Link>
-        </div>
-        <div className={activeMenu === "newsletter" ? styles.menuActive : styles.menuDiv}>
-          <Image width={18} height={18} src="newsletterIcon.svg" alt="Newsletter Icon" />
-          <Link
-            href="#"
-            onClick={() => {
-              handleOnClick("newsletter");
-            }}
-          >
-            Newsletter Creator
-          </Link>
-        </div>
-        <div className={activeMenu === "mailingList" ? styles.menuActive : styles.menuDiv}>
-          <Image width={18} height={18} src="mailIcon.svg" alt="Mail Icon" />
-          <Link
-            href="#"
-            onClick={() => {
-              handleOnClick("mailingList");
-            }}
-          >
-            Mailing List
-          </Link>
-        </div>
-        <div className={activeMenu === "settings" ? styles.menuActive : styles.menuDiv}>
-          <Image width={18} height={18} src="settingsIcon.svg" alt="Settings Icon" />
-          <Link
-            href="#"
-            onClick={() => {
-              handleOnClick("settings");
-            }}
-          >
-            Settings
-          </Link>
+        <div className={styles.user}>
+          <Image src={"sampleUserPhoto.svg"} alt="userImage" width={36} height={36}></Image>
+          <p>John Doe</p>
+          <Image src={"ic_caretdown.svg"} alt="upArrow" width={24} height={24}></Image>
         </div>
       </div>
-    </div>
+      <div className={styles.navBar}>
+        <Image
+          width={168.25}
+          height={59.69}
+          src="footerLogo.svg"
+          alt="4 Future Leaders of Tomorrow Logo"
+        />
+        <div className={styles.menu}>
+          <div className={activeMenu === "Dashboard" ? styles.menuActive : styles.menuDiv}>
+            <Image width={18} height={18} src="dashboardIcon.svg" alt="Dashboard Icon" />
+            <Link
+              href="/admin/dashboard"
+              onClick={() => {
+                handleOnClick("Dashboard");
+              }}
+            >
+              Dashboard
+            </Link>
+          </div>
+          <div className={activeMenu === "Event Creator" ? styles.menuActive : styles.menuDiv}>
+            <Image width={18} height={18} src="calendarIcon.svg" alt="Calendar Icon" />
+            <Link
+              href="#"
+              onClick={() => {
+                handleOnClick("Event Creator");
+              }}
+            >
+              Event Creator
+            </Link>
+          </div>
+          <div className={activeMenu === "Page Editor" ? styles.menuActive : styles.menuDiv}>
+            <Image width={18} height={18} src="pageIcon.svg" alt="Page Icon" />
+            <Link
+              href="/admin/pageeditor"
+              onClick={() => {
+                handleOnClick("Page Editor");
+              }}
+            >
+              Page Editor
+            </Link>
+          </div>
+          <div className={activeMenu === "Newsletter" ? styles.menuActive : styles.menuDiv}>
+            <Image width={18} height={18} src="newsletterIcon.svg" alt="Newsletter Icon" />
+            <Link
+              href="#"
+              onClick={() => {
+                handleOnClick("Newsletter");
+              }}
+            >
+              Newsletter Creator
+            </Link>
+          </div>
+          <div className={activeMenu === "Mailing List" ? styles.menuActive : styles.menuDiv}>
+            <Image width={18} height={18} src="mailIcon.svg" alt="Mail Icon" />
+            <Link
+              href="#"
+              onClick={() => {
+                handleOnClick("Mailing List");
+              }}
+            >
+              Mailing List
+            </Link>
+          </div>
+          <div className={activeMenu === "Settings" ? styles.menuActive : styles.menuDiv}>
+            <Image width={18} height={18} src="settingsIcon.svg" alt="Settings Icon" />
+            <Link
+              href="#"
+              onClick={() => {
+                handleOnClick("Settings");
+              }}
+            >
+              Settings
+            </Link>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 };
 
