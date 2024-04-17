80cf86609e86c6e1f601dca3f6d50023a8e47d4b
Jack Hansen <47556286+jackavh@users.noreply.github.com>
2024-04-16 22:26:23 -0700

    refactors DonateButton component to use new api endpoints in frontend/src/api/paypal.ts

--- frontend/src/components/payment/DonateButton.tsx
+++ frontend/src/components/payment/DonateButton.tsx
@@ -1,67 +1,65 @@
 import { PayPalButtons } from "@paypal/react-paypal-js";
-import { useEffect, useRef, useState } from "react";
-import { createPaypalOrder } from "@/api/paypal";
+import { redirect } from "next/navigation";
+import { useEffect, useRef } from "react";
 
-// Renders errors or successful transactions on the screen.
-function Message({ content }) {
-  return <p>{content}</p>;
-}
+import { OrderData, captureOrder, createOrder } from "@/api/paypal";
 
-export default function DonateButton({ productId }) {
-  const [message, setMessage] = useState("");
+type DonateButtonProps = {
+  productId: string;
+  quantity: string;
+};
+
+/**
+ * Types error details
+ * @see https://developer.paypal.com/docs/api/orders/v2/#definition-error_details
+ */
+type ErrorDetail = {
+  field: string;
+  value: string;
+  location: string;
+  issue: string;
+  description: string;
+};
+
+export default function DonateButton({ productId, quantity }: DonateButtonProps) {
   const productIdRef = useRef(productId);
+  const quantityRef = useRef(quantity);
 
   useEffect(() => {
     productIdRef.current = productId;
-  }, [productId]);
+    quantityRef.current = quantity;
+  }, [productId, quantity]);
 
   return (
     <div className="py-3">
       <PayPalButtons
         style={{ label: "donate" }}
-        createOrder={(data, actions) => {
-          
-        }}
+        createOrder={() =>
+          createOrder({ productId: productIdRef.current, quantity: quantityRef.current })
+        }
         onApprove={async (data, actions) => {
-          try {
-            const response = await fetch(`/api/orders/${data.orderID}/capture`, {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-            });
+          const orderData = captureOrder(data.orderID) as OrderData;
 
-            const orderData = await response.json();
-            // Three cases to handle:
-            //   (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
-            //   (2) Other non-recoverable errors -> Show a failure message
-            //   (3) Successful transaction -> Show confirmation or thank you message
+          // Three cases to handle:
+          //   (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
+          //   (2) Other non-recoverable errors -> Show a failure message
+          //   (3) Successful transaction -> Show confirmation or thank you message
+          const errorDetail = orderData?.details?.[0] as ErrorDetail; // if error, order.Data.details exists and the 0th element holds the highest precedence issue
 
-            const errorDetail = orderData?.details?.[0];
-
-            if (errorDetail?.issue === "INSTRUMENT_DECLINED") {
-              // (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
-              // recoverable state, per https://developer.paypal.com/docs/checkout/standard/customize/handle-funding-failures/
-              actions.restart(); return;
-            } else if (errorDetail) {
-              // (2) Other non-recoverable errors -> Show a failure message
-              throw new Error(`${errorDetail.description} (${orderData.debug_id})`);
-            } else {
-              // (3) Successful transaction -> Show confirmation or thank you message
-              // Or go to another URL:  actions.redirect('thank_you.html');
-              const transaction = orderData.purchase_units[0].payments.captures[0];
-              setMessage(
-                `Transaction ${transaction.status}: ${transaction.id}. See console for all available details`,
-              );
-              console.log("Capture result", orderData, JSON.stringify(orderData, null, 2));
-            }
-          } catch (error) {
-            console.error(error);
-            setMessage(`Sorry, your transaction could not be processed...${error}`);
+          if (errorDetail?.issue === "INSTRUMENT_DECLINED") {
+            // (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
+            // recoverable state, per https://developer.paypal.com/docs/checkout/standard/customize/handle-funding-failures/
+            actions.restart();
+            await Promise.resolve();
+          } else if (errorDetail) {
+            // (2) Other non-recoverable errors -> Show a failure message
+            throw new Error(`${errorDetail.description} (${orderData.debug_id})`);
+          } else {
+            // (3) Successful transaction -> Redirect user to success page
+            await Promise.resolve(redirect("/donations/success"));
           }
         }}
       />
-      <Message content={message} />
     </div>
   );
 }
