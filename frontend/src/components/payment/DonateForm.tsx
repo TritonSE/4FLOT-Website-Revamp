bc55c12f691c8f2e2536ffad6285632bb9218bd5
jennymar <jenmar528@gmail.com>
2024-05-06 17:10:43 -0700

    More frontend linting

--- frontend/src/components/payment/DonateForm.tsx
+++ frontend/src/components/payment/DonateForm.tsx
@@ -9,24 +9,24 @@ type DonateFormProps = {
 };
 
 export default function DonateForm({ setSuccess }: DonateFormProps) {
-  const [amount, setAmount] = useState("5.00");
+  const [amount, setAmount] = useState(5.0);
   const [amountUpdated, setAmountUpdated] = useState(false);
 
-  const handleAmountChange = (newAmount: string) => {
+  const handleAmountChange = (newAmount: number) => {
     setAmount(newAmount);
     setAmountUpdated(true);
   };
 
   // Initialize handleAmountChange once to load the donate button
   useEffect(() => {
-    handleAmountChange("5.00");
+    handleAmountChange(5.0);
   }, []);
 
   return (
     <form className="DonateForm">
       <AmountPicker onAmountChange={handleAmountChange} />
       <div className=" w-full pt-[16px]">
-        {amountUpdated && <DonateButton setSuccess={setSuccess} amount={amount} />}
+        {amountUpdated && <DonateButton setSuccess={setSuccess} amount={amount.toString()} />}
       </div>
     </form>
   );
