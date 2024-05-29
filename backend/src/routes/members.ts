32f2f3770716503bcf038c1dbdd6013c2b557781
katelynpdn <94756838+katelynpdn@users.noreply.github.com>
2024-05-29 11:46:52 -0700

    npm lintfix

--- backend/src/routes/members.ts
+++ backend/src/routes/members.ts
@@ -1,15 +1,13 @@
 import express from "express";
 import * as MembersController from "src/controllers/member";
-import * as MembersValidator from "src/validators/member"
+import * as MembersValidator from "src/validators/member";
 
 const router = express.Router();
 
 router.get("/get", MembersController.getAllMembers);
 router.get("/:id", MembersController.getMember);
 router.post("/post", MembersValidator.createMember, MembersController.createMember);
-router.put(
-    "/:id", MembersController.updateMember,
-  );
+router.put("/:id", MembersController.updateMember);
 router.delete("/:id", MembersController.deleteMember);
 
 export default router;
