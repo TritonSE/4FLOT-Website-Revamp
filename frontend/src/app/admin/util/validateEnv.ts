a62c2a93789106bd74f6d0e46519629c3ddf82fc
jennymar <jenmar528@gmail.com>
2024-06-01 10:44:29 -0700

    Firebase settings env

--- frontend/src/app/admin/util/validateEnv.ts
+++ frontend/src/app/admin/util/validateEnv.ts
@@ -4,7 +4,7 @@
  */
 
 import { cleanEnv } from "envalid";
-import { str } from "envalid/dist/validators";
+import { str, json } from "envalid/dist/validators";
 
 /**
  * NextJS only allows the frontend to access environment variables if they start with
@@ -19,7 +19,7 @@ export default cleanEnv(
   },
   {
     NEXT_PUBLIC_BACKEND_URL: str(), // URL of our backend
-    NEXT_PUBLIC_FIREBASE_SETTINGS: str(), // Firebase settings for frontend
+    NEXT_PUBLIC_FIREBASE_SETTINGS: json(), // Firebase settings for frontend
     NEXT_PUBLIC_PAYPAL_CLIENT_SECRET: str(),
     NEXT_PUBLIC_PAYPAL_CLIENT_ID: str(),
   },
